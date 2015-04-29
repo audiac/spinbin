180bd9143886b2b9609bd4e074441cfe8fd8f323
Ryan <ryan.geo@gmail.com>
2015-04-28 19:25:16 -0700

    Add login/logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Spinbin::Application.routes.draw do
   root 'welcome#index'
 
   get '/register', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
 
   resources :users, only: [:show, :create, :edit, :update]
 
