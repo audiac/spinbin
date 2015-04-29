302933340a176c7ee4217755cfe348b090de020a
Ryan <ryan.geo@gmail.com>
2015-04-29 14:52:20 -0700

    Add show action to collections_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       flash[:notice] = "Logged in successfully."
-      redirect_to root_path
+      redirect_to user_collections_path(user)
     else
       flash[:error] = "Error logging in."
       redirect_to login_path
