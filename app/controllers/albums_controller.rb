b00ec3092b21836d57f2bd710edfd35818631967
Ryan <ryan.geo@gmail.com>
2015-04-30 07:51:41 -0700

    Add ability to edit albums

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,9 +1,10 @@
 class AlbumsController < ApplicationController
 
   before_action :require_user
+  before_action :set_album, only: [:edit, :update]
+  before_action :set_collection, only: [:create, :edit, :update]
 
   def create
-    @collection = Collection.find(params[:collection_id])
     @album = @collection.albums.build(album_params)
 
     if @album.save
@@ -12,6 +13,19 @@ class AlbumsController < ApplicationController
       redirect_to user_collection_path(current_user.id, @collection)
     else
       @collection.albums.reload
+      render 'collections/show'
+    end
+  end
+
+  def edit
+    render 'collections/show'
+  end
+
+  def update
+    if @album.update(album_params)
+      flash[:notice] = "Album updated."
+      redirect_to user_collection_path(current_user.id, @collection)
+    else
       render 'collections/show'
     end
   end
@@ -27,6 +41,14 @@ class AlbumsController < ApplicationController
 
     def album_params
       params.require(:album).permit(:title, :artist, :release_year, :catalog_number, :record_label)
+    end
+
+    def set_album
+      @album = Album.find(params[:id])
+    end
+
+    def set_collection
+      @collection = Collection.find(params[:collection_id])
     end
 
 end
